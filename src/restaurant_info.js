a92daa8e9eb2c0bc768a0e110440a36e00b6c24c
AngieV <angiev@me.com>
2018-11-27 00:15:20 -0600

    Add files via upload
    update favorite button functionality

---
+++ src/restaurant_info.js
@@ -0,0 +1,251 @@
+import DBHelper from './dbhelper';
+import './register';
+import favoriteButton from './favorite-button';
+import reviewForm from './review-form';
+
+
+let restaurant;
+var newMap;
+
+/**
+ * Initialize map as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+  initMap();
+});
+
+/**
+ * Initialize leaflet map
+ */
+const initMap = () => {
+  fetchRestaurantFromURL((error, restaurant) => {
+    if (error) { // Got an error!
+      console.error(error);
+    } else {
+      newMap = L.map('map', {
+        center: [restaurant.latlng.lat, restaurant.latlng.lng],
+        zoom: 16,
+        scrollWheelZoom: false
+      });
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1IjoiYW5naWV2IiwiYSI6ImNqampwcDN6aTB1ZnozcW1kcmlzeTNxN3IifQ.ZCGR51fwOZyen-_d4_blkA',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+      }).addTo(newMap);
+      fillBreadcrumb();
+      DBHelper.mapMarkerForRestaurant(self.restaurant, newMap);
+    }
+  });
+}
+
+/**
+ * Get current restaurant from page URL.
+ */
+const fetchRestaurantFromURL = (callback) => {
+  if (self.restaurant) { // restaurant already fetched!
+    callback(null, self.restaurant)
+    return;
+  }
+  const id = getParameterByName('id');
+  if (!id) {
+  // no id found in URL
+    const error = 'No restaurant id in URL';
+    callback(error, null);
+  } else {
+    DBHelper.fetchRestaurantById(id, (error, restaurant) => {
+      self.restaurant = restaurant;
+      if (!restaurant) {
+        console.error(error);
+        return;
+      }
+      fillRestaurantHTML();
+      callback(null, restaurant)
+    });
+  }
+};
+
+/**
+ * Create restaurant HTML and add it to the webpage
+ */
+const fillRestaurantHTML = (restaurant = self.restaurant) => {
+  const div = document.getElementById('maincontent');
+  const name = document.getElementById('restaurant-name');
+  name.innerHTML = restaurant.name;
+
+  const address = document.getElementById('restaurant-address');
+  address.innerHTML = restaurant.address;
+
+  const image = document.getElementById('restaurant-img');
+  const imageUrl = DBHelper.imageUrlForRestaurant(restaurant);
+  image.className = 'restaurant-img';
+  if (imageUrl == "/img/na.png"){
+    image.src = "/img/na.png";
+    image.alt =  `No photo available`;
+  } else {
+    image.srcset =`${imageUrl}_sm.jpg 300w, ${imageUrl}_md.jpg 600w, ${imageUrl}_lg.jpg 800w`;
+    image.sizes = `90%vw, 300px`;
+    image.alt = `A photo of ${restaurant.name} restaurant with cuisine type ${restaurant.cuisine_type}`;
+  };
+
+  const cuisine = document.getElementById('restaurant-cuisine');
+  cuisine.innerHTML = restaurant.cuisine_type;
+
+  // fill operating hours
+  if (restaurant.operating_hours) {
+    fillRestaurantHoursHTML();
+  }
+
+  //create favorite div and add button
+  const ReviewsDiv = document.getElementById('reviews-container');
+  const favoriteDiv = document.createElement('div');
+  favoriteDiv.setAttribute('class', 'fav');
+  let favorite = favoriteButton(restaurant);
+  favorite.setAttribute('id', 'favBtn');
+  favoriteDiv.appendChild(favorite);
+  let isfaved = document.createElement('p');
+  isfaved.innerHTML = favorite.title;
+  favoriteDiv.appendChild(isfaved);
+
+  favorite.addEventListener('click', (event) => {
+    const newState = !restaurant.is_favorite;
+    DBHelper.handleFavoriteClick(favorite.restaurantId, newState);
+    restaurant.is_favorite = newState;
+
+    toggleFavorite(favorite, newState);
+    });
+
+  function toggleFavorite(el, fav) {
+    if(!fav){
+      el.classList.remove(".fav[aria-pressed=true]");
+      el.classList.add(".fav");
+      el.setAttribute('aria-label', 'click to favorite');
+      el.title = ` Click to Favorite`;
+    } else {
+      el.classList.remove(".fav");
+      el.classList.add(".fav[aria-pressed=true]");
+      el.setAttribute('aria-label', `Mark ${restaurant.name} as a favorite`);
+      el.title = ` ${restaurant.name} is a favorite!`;
+    }
+    favorite = el;
+    isfaved.innerHTML = el.title;
+  }
+
+  ReviewsDiv.append(favoriteDiv);
+  // fill reviews for stage 2 server
+  // fillReviewsHTML();
+  // fill reviews for stage 3 server
+  DBHelper.fetchReviewsByRestaurantId(restaurant.id, fillReviewsHTML);
+}
+
+/**
+ * Create restaurant operating hours HTML table and add it to the webpage.
+ */
+const fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
+  const hours = document.getElementById('restaurant-hours');
+  for (let key in operatingHours) {
+    const row = document.createElement('tr');
+
+    const day = document.createElement('td');
+    day.innerHTML = key;
+    day.setAttribute('class', 'day');
+    row.appendChild(day);
+
+    const time = document.createElement('td');
+    time.innerHTML = operatingHours[key];
+    time.setAttribute('class', 'time');
+    row.appendChild(time);
+
+    hours.appendChild(row);
+  }
+}
+
+/**
+ * Create all reviews HTML and add them to the webpage.
+ */
+const fillReviewsHTML = (error, reviews) => {
+  self.restaurant.reviews = reviews;
+  if(error){
+      console.log('Error retrieving reviews', error);
+    }
+  const container = document.getElementById('reviews-container');
+  const title = document.createElement('h2');
+  title.innerHTML = 'Reviews';
+  container.appendChild(title);
+
+  const h4 = document.createElement('h4');
+  h4.innerHTML = "Leave a Review";
+  container.appendChild(h4);
+  const id = getParameterByName('id');
+  container.appendChild(reviewForm(id));
+
+  if (!reviews) {
+    const noReviews = document.createElement('p');
+    noReviews.innerHTML = 'No reviews yet!';
+    container.appendChild(noReviews);
+    return;
+  }
+
+  const ul = document.getElementById('reviews-list');
+  reviews.reverse();
+  reviews.forEach(review => {
+    ul.appendChild(createReviewHTML(review));
+  });
+  container.appendChild(ul);
+}
+
+/**
+ * Create review HTML and add it to the webpage.
+ */
+const createReviewHTML = (review) => {
+  const li = document.createElement('li');
+  const name = document.createElement('p');
+  name.innerHTML = review.name;
+  name.setAttribute('class', 'name')
+  li.appendChild(name);
+
+  const date = document.createElement('p');
+  //insert Date and convert timestamp into readable format
+  date.innerHTML = new Date(review.createdAt).toLocaleDateString()
+  date.setAttribute('class','date');
+  li.appendChild(date);
+
+  const rating = document.createElement('p');
+  rating.innerHTML = `Rating: ${review.rating}`;
+  rating.setAttribute('class','rating');
+  li.appendChild(rating);
+
+  const comments = document.createElement('p');
+  comments.innerHTML = review.comments;
+  li.appendChild(comments);
+
+  return li;
+}
+
+/**
+ * Add restaurant name to the breadcrumb navigation menu
+ */
+const fillBreadcrumb = (restaurant=self.restaurant) => {
+  const breadcrumb = document.getElementById('breadcrumb');
+  const li = document.createElement('li');
+  li.innerHTML = restaurant.name;
+  breadcrumb.appendChild(li);
+}
+
+/**
+ * Get a parameter by name from page URL.
+ */
+const getParameterByName = (name, url) => {
+  if (!url)
+    url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
+    results = regex.exec(url);
+  if (!results)
+    return null;
+  if (!results[2])
+    return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
